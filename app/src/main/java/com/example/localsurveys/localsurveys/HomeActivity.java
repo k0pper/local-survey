65f0a070749c5415f5823146d61765db7adf543f
k0pper <mial1014@hs-karlsruhe.de>
2018-05-11 16:46:16 +0200

    Kommentar eingefügt

--- app/src/main/java/com/example/localsurveys/localsurveys/HomeActivity.java
+++ app/src/main/java/com/example/localsurveys/localsurveys/HomeActivity.java
@@ -13,6 +13,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+// Das ist ein Beispiel Kommentar
 public class HomeActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
