0cb12ac2b94cfe1673be90bc2715c82abd257609
Alexander Miller <mial1014@hs-karlsruhe.de>
2018-05-18 14:42:04 +0200

    Kommentare und Imports gelöscht

--- app/src/main/java/com/example/localsurveys/localsurveys/HomeActivity.java
+++ app/src/main/java/com/example/localsurveys/localsurveys/HomeActivity.java
@@ -1,9 +1,6 @@
 package com.example.localsurveys.localsurveys;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -13,7 +10,6 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-// Das ist ein Beispiel Kommentar
 public class HomeActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -23,15 +19,6 @@ public class HomeActivity extends AppCompatActivity
         setContentView(R.layout.activity_home_activity);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
