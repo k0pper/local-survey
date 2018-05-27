9efa8d24722efb857ce3896d1bd9006df8645276
Lisa131 <lisakmanger@gmail.com>
2018-05-27 12:09:40 +0200

    find view by id for addsurveyconfigurationfragment

--- app/src/main/java/com/example/localsurveys/localsurveys/addSurvey/AddSurveyConfigurationFragment.java
+++ app/src/main/java/com/example/localsurveys/localsurveys/addSurvey/AddSurveyConfigurationFragment.java
@@ -7,18 +7,29 @@ import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.SeekBar;
 
 import com.example.localsurveys.localsurveys.R;
 
-public class AddSurveyFragment extends Fragment {
+public class AddSurveyConfigurationFragment extends Fragment {
+
+    protected EditText titleEditTextView;
+    protected SeekBar radiusView;
+    protected EditText availabilityFromDateView;
+    protected EditText availabilityToDateView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(false);
 
-        View rootView = inflater.inflate(R.layout.fragment_add_survey, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_add_survey_configuration, container, false);
 
+        this.titleEditTextView = rootView.findViewById(R.id.add_survey__title_edit_text);
+        this.radiusView = rootView.findViewById(R.id.add_survey__radius_bar);
+        this.availabilityFromDateView = rootView.findViewById(R.id.add_survey__availability_from_date);
+        this.availabilityToDateView = rootView.findViewById(R.id.add_survey__availability_to_date);
 
         return rootView;
     }
