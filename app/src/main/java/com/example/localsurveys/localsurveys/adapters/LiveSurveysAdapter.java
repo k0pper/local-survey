3f7d9ffce95aeb2236b4d662199f0f7e2ecea669
k0pper <mial1014@hs-karlsruhe.de>
2018-06-19 10:04:11 +0200

    new layout model for seeing live surveys + Adapter

---
+++ app/src/main/java/com/example/localsurveys/localsurveys/adapters/LiveSurveysAdapter.java
@@ -0,0 +1,83 @@
+package com.example.localsurveys.localsurveys.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.localsurveys.localsurveys.R;
+import com.example.localsurveys.localsurveys.models.AnswerOption;
+import com.example.localsurveys.localsurveys.models.Question;
+import com.example.localsurveys.localsurveys.models.Survey;
+
+import java.util.ArrayList;
+
+/**
+ * Created by AlexanderMiller, on 19.06.2018.
+ */
+
+public class LiveSurveysAdapter extends BaseAdapter{
+    ArrayList<Survey> surveys;
+    Context c;
+    TextView titleTxt, questionsTxt, creatorTxt, publishedTxt;
+
+    @Override
+    public int getCount() {
+        return surveys.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return surveys.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(c).inflate(R.layout.participate_survey_model, parent, false);
+        }
+
+        final Survey s = (Survey) this.getItem(position);
+        titleTxt = convertView.findViewById(R.id.title_txt);
+        questionsTxt = convertView.findViewById(R.id.question_txt);
+        creatorTxt = convertView.findViewById(R.id.creatorTxt);
+        publishedTxt = convertView.findViewById(R.id.publishTxt);
+
+        titleTxt.setText(s.getTitle());
+        questionsTxt.setText("Questions: " + s.getQuestions().size());
+        creatorTxt.setText("Testemail");
+
+        // Calculate Time since Publishing
+        long now = System.currentTimeMillis();
+        long surveyStart = s.getFromDate();
+        long difference = now - surveyStart;
+        String unit;
+        int result;
+        if (difference < 60000) {
+            unit = "Seconds";
+            result = (int) difference / 1000;
+        } else {
+            unit = "Minutes";
+            result = (int) difference / 60000;
+        }
+        publishedTxt.setText("Published " + result + " " + unit + " ago");
+        creatorTxt.setText("Testemail");
+
+
+        convertView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        return convertView;
+    }
+}
